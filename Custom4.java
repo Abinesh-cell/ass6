82671b95a2d4178edd459e8230a85deef27dfdd9
Abinesh-cell <abinesh.s2024@vitstudent.ac.in>
2026-09-10 14:40:46 +0530

    Create  Custom4.java

---
+++ Custom4.java
@@ -0,0 +1,5 @@
+package com.dispatch.exception;
+
+public class InvalidEmergencyRequestException extends Exception {
+    public InvalidEmergencyRequestException(String message) { super(message); }
+}
